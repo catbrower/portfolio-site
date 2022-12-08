9e600cd20189d5299f65adad0e12a1fd346ab4bd
Cathewrine Brower <catherinemb42@gmail.com>
2022-12-07 23:04:01 -0500

    changing resume animation to use an adj matrix

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,18 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect} from "react";
 
 import * as THREE from 'three';
 
 import {
-    AppBar, 
-    Box,
-    Button,
     Container,
-    IconButton,
-    Stack,
-    Toolbar,
-    Typography,
-    Fade,
-    Grow
+    Stack
 } from '@mui/material';
 
 import PageItem from '../components/PageItem';
@@ -23,6 +15,7 @@ import Bullet from "../components/Bullet";
 
 export default function Resume(props) {
     let group;
+    let drawRange = 0;
     const particlesData = [];
     let camera, scene, renderer;
     let positions, colors;
@@ -33,12 +26,11 @@ export default function Resume(props) {
 
     let fadeIn = 0;
     let start = -1;
-    const maxParticleCount = 1000;
     let particleCount = 100;
     let particleSpeed = .1;
     let rotationSpeedY = 0.005;
     let rotationSpeedZ = 0.01;
-    const r = 1000;
+    const r = 750;
     const rHalf = r / 2;
 
     let lineColor = 0x050505;
@@ -47,14 +39,24 @@ export default function Resume(props) {
     const effectController = {
         showDots: false,
         showLines: true,
-        minDistance: 200,
-        limitConnections: true,
-        maxConnections: 20,
+        minDistance: 500000,
+        maxConnections: 5,
         particleCount: 500
     };
 
-    init();
-    animate();
+    //New stuff
+    const N = 500;
+    const p = 0.01;
+    let adjMatrix = new Array(N);
+    for(let i = 0; i < N; i++) {
+        let row = new Array(N);
+        for(let j = 0; j < N; j++) {
+            row[j] = 0;
+        }
+        adjMatrix[i] = row;
+    }
+
+    let maxParticleCount = N;
 
     function init() {
         let canvas = document.getElementsByClassName('resumeCanvas')[0];
@@ -65,7 +67,7 @@ export default function Resume(props) {
         renderer.outputEncoding = THREE.sRGBEncoding;
 
         camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 4000);
-        camera.position.z = 750;
+        camera.position.z = 1000;
 
         scene = new THREE.Scene();
 
@@ -83,48 +85,45 @@ export default function Resume(props) {
         positions = new Float32Array(segments * 3);
         colors = new Float32Array(segments * 3);
 
-        const pMaterial = new THREE.PointsMaterial( {
+        const pMaterial = new THREE.PointsMaterial({
             color: pointColor,
             size: 0,
             // blending: THREE.AdditiveBlending,
             // transparent: true,
             sizeAttenuation: false
-        } );
+        });
 
         particles = new THREE.BufferGeometry();
-        particlePositions = new Float32Array( maxParticleCount * 3 );
+        particlePositions = new Float32Array(maxParticleCount * 3);
 
-        for ( let i = 0; i < maxParticleCount; i ++ ) {
+        // Set initial positions
+        for (let i = 0; i < maxParticleCount; i ++) {
             const x = Math.random() * r - r / 2;
             const y = Math.random() * r - r / 2;
             const z = Math.random() * r - r / 2;
 
-            particlePositions[ i * 3 ] = x;
-            particlePositions[ i * 3 + 1 ] = y;
-            particlePositions[ i * 3 + 2 ] = z;
+            particlePositions[i * 3] = x;
+            particlePositions[i * 3 + 1] = y;
+            particlePositions[i * 3 + 2] = z;
 
             // add it to the geometry
             particlesData.push( {
                 velocity: new THREE.Vector3(Math.random() * particleSpeed, Math.random() * particleSpeed, Math.random() * particleSpeed),
-                numConnections: 0
+                // numConnections: 0
             } );
         }
 
-        particles.setDrawRange( 0, particleCount );
+        // particles.setDrawRange( 0, particleCount );
         particles.setAttribute( 'position', new THREE.BufferAttribute( particlePositions, 3 ).setUsage( THREE.DynamicDrawUsage ) );
 
         // create the particle system
-        pointCloud = new THREE.Points( particles, pMaterial );
-        group.add( pointCloud );
+        pointCloud = new THREE.Points(particles, pMaterial);
+        group.add(pointCloud);
 
         const geometry = new THREE.BufferGeometry();
-
-        geometry.setAttribute( 'position', new THREE.BufferAttribute( positions, 3 ).setUsage( THREE.DynamicDrawUsage ) );
-        geometry.setAttribute( 'color', new THREE.BufferAttribute( colors, 3 ).setUsage( THREE.DynamicDrawUsage ) );
-
+        geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3).setUsage(THREE.DynamicDrawUsage));
+        geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3).setUsage(THREE.DynamicDrawUsage));
         geometry.computeBoundingSphere();
-
-        geometry.setDrawRange( 0, 0 );
 
         const material = new THREE.LineBasicMaterial( {
             color: lineColor,
@@ -133,86 +132,79 @@ export default function Resume(props) {
             transparent: true
         } );
 
-        linesMesh = new THREE.LineSegments( geometry, material );
-        group.add( linesMesh );
+        linesMesh = new THREE.LineSegments(geometry, material);
+        group.add(linesMesh);
+
+        // Determine initial connections
+        for(let i = 0; i < N; i++) {
+            for(let j = 0; j < i; j++) {
+                let connections = 0;
+                const dx = particlePositions[i * 3] - particlePositions[j * 3];
+                const dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
+                const dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
+
+                const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+                // if(dist < effectController.minDistance && connections < effectController.maxConnections) {
+                    adjMatrix[i][j] = 1;
+                    adjMatrix[j][i] = 1;
+                    // connections += 1;
+                // }
+            }
+        }    
     }
 
     function animate() {
         let vertexpos = 0;
         let colorpos = 0;
-        let numConnected = 0;
-
-        for ( let i = 0; i < particleCount; i ++ )
-            particlesData[ i ].numConnections = 0;
 
         for ( let i = 0; i < particleCount; i ++ ) {
-
             // get the particle
             const particleData = particlesData[ i ];
 
-            particlePositions[ i * 3 ] += particleData.velocity.x;
-            particlePositions[ i * 3 + 1 ] += particleData.velocity.y;
-            particlePositions[ i * 3 + 2 ] += particleData.velocity.z;
+            particlePositions[i * 3] += particleData.velocity.x;
+            particlePositions[i * 3 + 1] += particleData.velocity.y;
+            particlePositions[i * 3 + 2] += particleData.velocity.z;
 
-            if ( particlePositions[ i * 3 + 1 ] < - rHalf || particlePositions[ i * 3 + 1 ] > rHalf )
+            if(particlePositions[i * 3 + 1] < - rHalf || particlePositions[i * 3 + 1] > rHalf) {
                 particleData.velocity.y = - particleData.velocity.y;
+            }
 
-            if ( particlePositions[ i * 3 ] < - rHalf || particlePositions[ i * 3 ] > rHalf )
+            if(particlePositions[i * 3] < - rHalf || particlePositions[i * 3] > rHalf) {
                 particleData.velocity.x = - particleData.velocity.x;
+            }
 
-            if ( particlePositions[ i * 3 + 2 ] < - rHalf || particlePositions[ i * 3 + 2 ] > rHalf )
+            if(particlePositions[i * 3 + 2] < - rHalf || particlePositions[i * 3 + 2] > rHalf) {
                 particleData.velocity.z = - particleData.velocity.z;
-
-            if ( effectController.limitConnections && particleData.numConnections >= effectController.maxConnections )
-                continue;
-
+            }
+                
             // Check collision
-            for ( let j = i + 1; j < particleCount; j ++ ) {
-                const particleDataB = particlesData[ j ];
-                if (effectController.limitConnections && particleDataB.numConnections >= effectController.maxConnections )
-                    continue;
+            for (let j = 0; j < i; j++) {
+                if(adjMatrix[i][j] === 1) {
+                    positions[i] = particlePositions[i * 3];
+                    positions[i + 1] = particlePositions[i * 3 + 1];
+                    positions[i + 2] = particlePositions[i * 3 + 2];
 
-                const dx = particlePositions[ i * 3 ] - particlePositions[ j * 3 ];
-                const dy = particlePositions[ i * 3 + 1 ] - particlePositions[ j * 3 + 1 ];
-                const dz = particlePositions[ i * 3 + 2 ] - particlePositions[ j * 3 + 2 ];
-                const dist = Math.sqrt( dx * dx + dy * dy + dz * dz );
+                    positions[j] = particlePositions[j * 3];
+                    positions[j + 1] = particlePositions[j * 3 + 1];
+                    positions[j + 2] = particlePositions[j * 3 + 2];
 
-                if (dist < effectController.minDistance) {
-                    particleData.numConnections ++;
-                    particleDataB.numConnections ++;
+                    colors[colorpos++] = fadeIn;
+                    colors[colorpos++] = fadeIn;
+                    colors[colorpos++] = fadeIn;
 
-                    let alpha = fadeIn;
-
-                    positions[ vertexpos ++ ] = particlePositions[ i * 3 ];
-                    positions[ vertexpos ++ ] = particlePositions[ i * 3 + 1 ];
-                    positions[ vertexpos ++ ] = particlePositions[ i * 3 + 2 ];
-
-                    positions[ vertexpos ++ ] = particlePositions[ j * 3 ];
-                    positions[ vertexpos ++ ] = particlePositions[ j * 3 + 1 ];
-                    positions[ vertexpos ++ ] = particlePositions[ j * 3 + 2 ];
-
-                    colors[ colorpos ++ ] = alpha;
-                    colors[ colorpos ++ ] = alpha;
-                    colors[ colorpos ++ ] = alpha;
-
-                    colors[ colorpos ++ ] = alpha;
-                    colors[ colorpos ++ ] = alpha;
-                    colors[ colorpos ++ ] = alpha;
-
-                    numConnected ++;
+                    colors[colorpos++] = fadeIn;
+                    colors[colorpos++] = fadeIn;
+                    colors[colorpos++] = fadeIn;
                 }
             }
         }
-
-
-        linesMesh.geometry.setDrawRange( 0, numConnected * 2 );
+        
         linesMesh.geometry.attributes.position.needsUpdate = true;
         linesMesh.geometry.attributes.color.needsUpdate = true;
-
         pointCloud.geometry.attributes.position.needsUpdate = true;
 
-        requestAnimationFrame( animate );
-
+        requestAnimationFrame(animate);
         render();
     }
 
@@ -230,19 +222,15 @@ export default function Resume(props) {
 
         group.rotation.y = time * rotationSpeedY;
         group.rotation.z = time * rotationSpeedZ;
-        renderer.render( scene, camera );
+        renderer.render(scene, camera);
     }
 
     function handleResize() {
-        // camera.aspect = window.innerWidth / window.innerHeight;
-        // camera.updateProjectionMatrix();
-
         renderer.setSize( window.innerWidth, window.innerHeight );
     }
 
     useEffect(() => {
         window.addEventListener('resize', handleResize);
-        // window.addEventListener('scroll', handleScroll);
         
         init();
         animate();
