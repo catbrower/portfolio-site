ba8d72dddf319c83f40739347ba3240fedceb79b
Catherine Brower <catherinemb42@gmail.com>
2023-03-09 01:39:43 -0500

    redesigning

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+https://bashooka.com/coding/css-geometric-animation-examples/
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -108,12 +108,7 @@ class Contact extends Component {
               <div className="widget widget_contact">
                 <h4>Address and Phone</h4>
                 <p className="address">
-                  {name}
-                  <br />
-                  {street} <br />
-                  {city}, {state} {zip}
-                  <br />
-                  <span>{phone}</span>
+                  put something else here
                 </p>
               </div>
             </aside>
